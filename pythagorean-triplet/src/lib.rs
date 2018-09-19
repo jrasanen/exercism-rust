38bb368adaad7e7db29760a51ebc1aede2167835
Jussi Räsänen <jussir@gmail.com>
2018-09-19 23:23:05 +0300

    change target to variable

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -4,7 +4,7 @@ pub fn find() -> Option<u32> {
     for a in 1..=target/3 {
         for b in (a+1)..=target/2 {
             let c: u32 = target-a-b;
-            if a*a + b*b == c*c && a + b + c == 1000 {
+            if a*a + b*b == c*c && a + b + c == target {
                 return Some(a*b*c);
             }
         }
